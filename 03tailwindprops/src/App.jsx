5c8b4645f9314a1b8d9376cf8ac32b2ee64ccfa9
Wasim <wasimbhai6205@gmail.com>
2023-09-23 23:43:02 -0700

    props and tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+
+  let myObj = {
+    username: "wasim",
+    age: 18
+  }
+  let newArr = [1,2,3]
+
+  return (
+    <>
+      <h1 className="bg-green-400 teat-black p-4 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card username="wasim" btnText="click me"/>
+      <Card username="hitesh" btnText="visit me"/>
+    </>
+  );
+}
+
+export default App;
